1c8202eecd00a4f8ef55565e5a782fbe7250443f
sonu <sonu@gmail.com>
2024-02-03 18:55:14 +0530

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM tomcat:9
-copy target/*war /usr/local/tomcat/webapps
+COPY target/*war /usr/local/tomcat/webapps
--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -7,15 +7,52 @@ pipeline{
             steps{
                dir("${WORKSPACE}"){
                  script{
+                    cleanWs()
                     println('checkout git repo')
                     sh"""
                         git clone https://github.com/shubhamshiyale/${GIT-repo}.git
-                        
+
                     """
                  }
                }
             }
            
+        }
+        stage('build'){
+            steps{
+                dir("${WORKSPACE}"){
+                script{
+                    sh """
+                       cd ${GIT-repo}
+                       mvn install
+                    """
+                }
+                }
+            }
+        }
+        stage('build image'){
+            steps{
+                dir("${WORKSPACE}"){
+                    script{
+                        sh """
+                           cd ${GIT-repo}
+                           docker build -t ${GIT-repo}-image .
+                        """
+                    }
+                }
+            }
+        }
+        stage('deploy'){
+            steps{
+                dir("${WORKSPACE}"){
+                    script{
+                        sh"""
+                          docker images 
+                          docker run -itdp 8080:8080 ${GIT-repo}-image                           
+                        """
+                    }
+                }
+            }
         }
     }
 }
