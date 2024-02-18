d80173e55911d6269b171156739387861f4f5890
sonu <sonu@gmail.com>
2024-02-18 21:03:02 +0530

    cicd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,2 +1,17 @@
 // This is the jenkins-file to cicd
 //this is a second commit 
+pipeline{
+    agent any
+    stages{
+        stage('stage-checkout'){
+            steps{
+                script{
+                    dir("${WORKSPACE}"){
+                    println 'git checkout started'
+                    clone = "git clone https://github.com/shubhamshiyale/maven-project-war-file.git".execute()
+                    }
+                }
+            }
+        }
+    }
+}
